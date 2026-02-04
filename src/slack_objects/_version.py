226e05504d4d70c8046c7c3c941ea734f5312eff
Mercado, Marcos <Marcos.Mercado@activision.com>
2026-02-04 14:18:23 -0800

    updated release

--- src/slack_objects/_version.py
+++ src/slack_objects/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.post31'
-__version_tuple__ = version_tuple = (0, 0, 'post31')
+__version__ = version = '0.0.post34'
+__version_tuple__ = version_tuple = (0, 0, 'post34')
 
-__commit_id__ = commit_id = 'gcf17794dd'
+__commit_id__ = commit_id = 'g37a2879de'
